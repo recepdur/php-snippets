6c13ed2d73ad0463e2609bd761c9bceb94ac8a03
Recep Dur <recepdur@outlook.com>
2019-05-09 15:20:02 +0300

    Update oracle-connection.php

--- oracle-connection.php
+++ oracle-connection.php
@@ -1,5 +1,7 @@
 <?php
+    
     $conn = oci_connect("username", "password", "(DESCRIPTION =(ADDRESS_LIST =(ADDRESS = (COMMUNITY = tcp.world)(PROTOCOL = TCP)(Host = 192.168.1.45)(Port = 1521)))(CONNECT_DATA = (SID = ORAVT)))");
+    
     if($conn != null)
     {
         //echo "Bağlandı.";
@@ -9,4 +11,18 @@
         $err = OCIError();
         echo "Bağlanamadı. ";
     }      
+
+    $query = oci_parse($conn, "select * from USER where USER_ID='$user_id'");
+    oci_execute($query);               
+    while($row = oci_fetch_array($query))
+    {
+        echo $row["USER_NAME"];
+    } 
+    $query = oci_parse($conn, "insert into USER (USER_NAME, USER_PASSWORD) VALUES('$kullanici_ad','$kullanici_md5_sifre')");
+    oci_execute($query); 
+    $query = oci_parse($conn, "delete from USER where USER_ID='$user_id'");
+    oci_execute($query);
+    $query = oci_parse($conn, "update USER set USER_PASSWORD='$kullanici_md5_sifre' where USER_ID='$user_id'");
+    oci_execute($query); 
+
 ?>
